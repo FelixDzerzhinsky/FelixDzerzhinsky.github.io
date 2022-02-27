6c4fcba585a2e5b5e008d49d11a22dc6b74457c0
FelixDzerzhinsky <68434958+FelixDzerzhinsky@users.noreply.github.com>
2022-02-27 13:26:00 +0500

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-   $('.fairy__tail__slaider').slick({
-       prevArrow: '<button type="button" class="slick-prev"><img src="images/back.svg" alt=""></button>',
-       nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
-       autoplay: 3000
-   });
-});
